5124997f8c3b3610f53925c00be9dc29230a791d
suganya05 <101466647+suganya05@users.noreply.github.com>
2023-11-27 19:05:35 +0530

    pull

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import About from "./components/About";
 import Footer from "./components/Footer";
+import VideoPromo from "./components/VideoPromo";
 
 const App: React.FC = () => {
   return (
     <div>
       <About />
+      <VideoPromo />
       <Footer />
     </div>
   );
