6546249d0fd0c56ce10af4f9901a6e7c45c399dc
suganya05 <101466647+suganya05@users.noreply.github.com>
2023-11-28 22:27:26 +0530

    pull

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,25 +1,38 @@
 import React from "react";
-import "./About.scss";
 import { AboutData } from "../../data/aboutData";
+import RectangleImg from "../../assets/images/Rectangle.png";
+import Arrow from "../../assets/images/arrow.png";
+import "./About.scss";
 
 const About: React.FC = () => {
   return (
     <div className="about-wrapper">
       <div className="mx">
+        <div className="rectangle-img">
+          <img src={RectangleImg} alt="" />
+        </div>
+        <div>
+          <div className="border-radius"></div>
+          <div className="arrow-img">
+            <img src={Arrow} alt="" />
+          </div>
+        </div>
         <div className="background-img">
-          {AboutData.map((f, index) => {
-            return (
-              <div key={index} className="about-container">
-                <div className="image">
-                  <img src={f.image} alt="" />
+          <div>
+            {AboutData.map((f, index) => {
+              return (
+                <div key={index} className="about-container">
+                  <div className="image">
+                    <img src={f.image} alt="" />
+                  </div>
+                  <div className="about-content">
+                    <h1>{f.title}</h1>
+                    <p>{f.description}</p>
+                  </div>
                 </div>
-                <div className="about-content">
-                  <h1>{f.title}</h1>
-                  <p>{f.description}</p>
-                </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
