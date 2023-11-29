b3e6d413f522c2374929878640dc63107b49ad93
suganya05 <101466647+suganya05@users.noreply.github.com>
2023-11-29 12:01:11 +0530

    pull

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { AboutData } from "../../data/aboutData";
 import RectangleImg from "../../assets/images/Rectangle.png";
 import Arrow from "../../assets/images/arrow.png";
 import "./About.scss";
 
 const About: React.FC = () => {
+  const backgroundRef = useRef<HTMLDivElement>(null);
+  const [expanded, setExpanded] = useState(false);
+
+  const handleArrowClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <div className="about-wrapper">
+    <div className={`about-wrapper ${expanded ? "expanded" : "add"}`}>
       <div className="mx">
         <div className="rectangle-img">
           <img src={RectangleImg} alt="" />
         </div>
-        <div>
+        <div onClick={handleArrowClick}>
           <div className="border-radius"></div>
           <div className="arrow-img">
             <img src={Arrow} alt="" />
           </div>
         </div>
-        <div className="background-img">
+        <div
+          className="background-img"
+          ref={backgroundRef}
+          style={{
+            height: expanded ? `${backgroundRef.current?.scrollHeight}px` : "0",
+            transition: "all 0.5s ease-out",
+          }}
+        >
           <div>
             {AboutData.map((f, index) => {
               return (
